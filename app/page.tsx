d60f160b57d7827210f3f7a3a3bea8d3bb9548a1
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-31 20:59:16 +0530

    chat input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import ChatInput from "@/components/ChatInput";
+import MessageList from "@/components/MessageList";
+
 export default function Home() {
-	return <main className=""></main>;
+	return (
+		<main className="">
+			{/* Message List */}
+			<MessageList />
+
+			{/* Chat Input */}
+			<ChatInput />
+		</main>
+	);
 }
---
+++ components/MessageList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = {};
+
+const MessageList = (props: Props) => {
+	return (
+		<div>
+			<p>Message</p>
+			<p>Message</p>
+			<p>Message</p>
+			<p>Message</p>
+			<p>Message</p>
+			<p>Message</p>
+			<p>Message</p>
+		</div>
+	);
+};
+
+export default MessageList;
