561a0cb4e364803fe71060405b4f58b49e78df9e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-31 20:38:52 +0530

    header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-	return (
-		<main className="flex">
-			<h1 className="text-2xl font-bold text-red-400 border-2 p-5 border-green-500 m-5">
-				Let's build Meta Messenger
-			</h1>
-		</main>
-	);
+	return <main className=""></main>;
 }
