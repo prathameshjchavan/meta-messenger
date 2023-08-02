ce52c0dc6388a0ab35a2091f2109bff505a3660e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-08-02 13:41:04 +0530

    nextauth authentication with facebook

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+
+const handler = NextAuth({
+	providers: [
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_CLIENT_ID!,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+		}),
+	],
+	secret: process.env.NEXTAUTH_SECRET!,
+	pages: {
+		signIn: "/auth/signin",
+	},
+});
+
+export { handler as GET, handler as POST };
---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,25 @@
+import SignIn from "@/components/SignIn";
+import { getProviders } from "next-auth/react";
+import Image from "next/image";
+
+const SignInPage = async () => {
+	const providers = await getProviders();
+
+	return (
+		<div>
+			<div>
+				<Image
+					src="https://links.papareact.com/161"
+					className="rounded-full mx-2 object-cover"
+					width={700}
+					height={700}
+					alt="Profile Picture"
+				/>
+			</div>
+
+			<SignIn providers={providers} />
+		</div>
+	);
+};
+
+export default SignInPage;
